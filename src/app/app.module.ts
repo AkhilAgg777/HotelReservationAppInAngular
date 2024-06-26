55ae27d09e6bb28ae53a4731ad20c781da6b380d
Akhil Aggarwal <akhilaggarwal7790@gmail.com>
2024-06-26 15:22:50 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
+import { ReservationModule } from './reservation/reservation.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule,
+    ReservationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
